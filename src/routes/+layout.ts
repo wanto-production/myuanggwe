b7e3ea11ea60150472d6464fd1118ef9b7a5283c
wanto-production <ikwansatria3974@gmail.com>
2026-02-10 20:10:08 +0700

    update

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,6 @@
 import { QueryClient } from '@tanstack/svelte-query'
 import { browser } from '$app/environment'
-import gsap from "gsap";
 import { client } from '$lib/eden.js';
-import { ScrollTrigger } from 'gsap/all';
 
 export const load = async ({ fetch, depends }) => {
   depends("layout:data")
@@ -15,7 +13,6 @@ export const load = async ({ fetch, depends }) => {
     },
   })
 
-  gsap.registerPlugin(ScrollTrigger)
 
   try {
 
