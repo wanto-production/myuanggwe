d72cd7ab1d475b75bc59b7af2be3c0bfc542c2b5
wanto-production <ikwansatria3974@gmail.com>
2026-03-02 17:11:21 +0700

    update

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,35 +1,55 @@
 import { PUBLIC_APP_BASE_URL } from '$env/static/public'
 
-const pages = [
-  { url: '', priority: 1.0, changefreq: 'daily' as const },
-  { url: 'login', priority: 0.8, changefreq: 'monthly' as const },
-  { url: 'register', priority: 0.8, changefreq: 'monthly' as const }
+type PageItem = {
+	loc: string
+	lastmod?: string
+	changefreq: 'always' | 'hourly' | 'daily' | 'weekly' | 'monthly' | 'yearly' | 'never'
+	priority: number
+}
+
+const staticPages: PageItem[] = [
+	{
+		loc: '',
+		changefreq: 'daily',
+		priority: 1.0
+	},
+	{
+		loc: 'login',
+		changefreq: 'monthly',
+		priority: 0.8
+	},
+	{
+		loc: 'register',
+		changefreq: 'monthly',
+		priority: 0.8
+	}
 ]
 
 export async function GET() {
-  return new Response(generateSitemap(), {
-    headers: {
-      'Content-Type': 'application/xml',
-      'Cache-Control': 'public, max-age=3600'
-    }
-  })
+	const sitemap = generateSitemap(staticPages)
+
+	return new Response(sitemap, {
+		headers: {
+			'Content-Type': 'application/xml; charset=utf-8',
+			'Cache-Control': 'public, max-age=3600'
+		}
+	})
 }
 
-function generateSitemap() {
-  const now = new Date().toISOString()
+function generateSitemap(pages: PageItem[]): string {
+	const lastmod = new Date().toISOString()
 
-  return `<?xml version="1.0" encoding="UTF-8"?>
-<?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>
+	return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${pages
-      .map(
-        ({ url, priority, changefreq }) => `  <url>
-    <loc>${PUBLIC_APP_BASE_URL}${url ? `/${url}` : ''}</loc>
-    <lastmod>${now}</lastmod>
-    <changefreq>${changefreq}</changefreq>
-    <priority>${priority}</priority>
+	.map(
+		page => `  <url>
+    <loc>${PUBLIC_APP_BASE_URL}${page.loc ? `/${page.loc}` : ''}</loc>
+    <lastmod>${page.lastmod || lastmod}</lastmod>
+    <changefreq>${page.changefreq}</changefreq>
+    <priority>${page.priority}</priority>
   </url>`
-      )
-      .join('\n')}
+	)
+	.join('\n')}
 </urlset>`
 }
