62ba6916c39fec14d493c870e8ec5f68a1eb1c7e
wanto-production <ikwansatria3974@gmail.com>
2026-03-19 17:51:29 +0700

    update

--- src/lib/composables/eden.ts
+++ src/lib/composables/eden.ts
@@ -1,5 +1,5 @@
 import { treaty } from '@elysiajs/eden'
 import type { App } from '$lib/server/rest-api'
-import { PUBLIC_APP_BASE_URL } from '$env/static/public'
+import { browser } from '$app/environment'
 
-export const client = treaty<App>(PUBLIC_APP_BASE_URL).api
+export const client = treaty<App>(browser ? location.origin : "").api
